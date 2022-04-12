17f90b7d8d38d898b5e62d998e03ca5da0f5352d
minerva365 <kyla.minerva@outlook.com>
2022-04-12 10:20:40 +0700

    first commit

---
+++ src/atm/Atm.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atm;
+import form.LoginFrame;
+
+/**
+ *
+ * @author RIG-1
+ */
+public class Atm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // TODO code application logic here
+        LoginFrame Frame= new LoginFrame();
+        Frame.setLocationRelativeTo(null);
+        Frame.setVisible(true);
+        Frame.setResizable(false);
+        
+    }
+    
+}
